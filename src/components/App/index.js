17a4b328768d6b1f9423bc8da66c97f737b76303
Sergej <sergej@computerassitance.co.uk>
2018-11-04 17:21:38 +0000

    fetch api to get tv series

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
-import Intro from '../Intro';
 
+import Intro from '../Intro';
+import Series from '../../containers/Series';
+import './App.css';
+import 'whatwg-fetch';
 
 class App extends Component {
 
-  state = {
-    series: []
-  }
-
-  componentDidMount() {
-    const series = ["Vikings"," Titanik", "gAm of thrones"];
-    setTimeout( () => {
-      this.setState({ series })
-    },2000);    
-  }
+ 
   render() {
     return (
       <div className="App">
@@ -27,7 +20,8 @@ class App extends Component {
         </header>
 
         <Intro message="Here u can find all of your loved series" />
-        the length of series array: {this.state.series.length}
+        
+        <Series />
       </div>
     );
   }
---
+++ src/containers/Series/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import SeriesList from '../../components/SeriesList';
+
+class Series extends Component {
+    
+    state = {
+        series: []
+      }
+    
+    componentDidMount() {
+    fetch('http://api.tvmaze.com/search/shows?q=Vikings')
+        .then((response) => response.json())
+        .then(json => this.setState({ series: json }))
+    }
+
+    render() {
+        return (
+            <div> 
+                the length of series array: {this.state.series.length}    
+                <SeriesList list ={this.state.series} />
+            </div>
+        )
+    }
+}
+
+export default Series;
